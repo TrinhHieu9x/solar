dc3712d01ea7fca81a98ec09a24d46031d589cc8
TrinhHieu9x <trinhdinhhieu96@gmail.com>
2025-08-29 12:41:23 +0700

    Update inverter_20240906.js

--- WManage_files/inverter_20240906.js
+++ WManage_files/inverter_20240906.js
@@ -482,7 +482,7 @@ function getParallelGroupDetails() {
 							var trHtml = '<tr>';
 							trHtml += '<td>' + element.serialNum + '</td>';
 							trHtml += '<td>' + (element.lost || (!element.vpv1 && element.vpv1 != 0) ? '' : (element.vpv1 ) + 'V') + '</td>';
-							trHtml += '<td>' + (element.lost || (!element.ppv1 && element.ppv1 != 0) ? '' : (element.ppv1 ) + 'W') + '</td>';
+							trHtml += '<td>' + (element.lost || (!element.ppv1 && element.ppv1 != 0) ? '' : (element.ppv1 ).toFix(1) + 'W') + '</td>';
 							trHtml += '<td>' + (element.lost || (!element.vpv2 && element.vpv2 != 0) ? '' : (element.vpv2 ) + 'V') + '</td>';
 							trHtml += '<td>' + (element.lost || (!element.ppv2 && element.ppv2 != 0) ? '' : element.ppv2 + 'W') + '</td>';
 
