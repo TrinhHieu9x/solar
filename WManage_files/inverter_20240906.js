0c698879213f2451be9d5d90643f43c0c1e8ff3c
TrinhHieu9x <trinhdinhhieu96@gmail.com>
2025-09-07 09:59:47 +0700

    Update inverter_20240906.js

--- WManage_files/inverter_20240906.js
+++ WManage_files/inverter_20240906.js
@@ -1752,7 +1752,7 @@ function refreshInverterInformationSingle(snForRefreshInfo) {
 				}
 			}
 
-			if(response.directions.inverterArrowDir > 0 && response.pDisCharge != 0 || response.ppv1 *1000 > 0 ) {
+			if(response.directions.inverterArrowDir > 0 && response.pDisCharge != 0 || response.ppv1 *1000 > 0 && response.gridPower > 0) {
 				$('.flowChartHolder[chartTarget=' + snForRefreshInfo + '] img.inverterArrayImg').attr('src', resourceBaseUrl + '/' + platformUrl + '/img/monitor/plant/arrow/_arrow_right.gif').show();
 			} else if(response.pDisCharge == 0 ) {
 				$('.flowChartHolder[chartTarget=' + snForRefreshInfo + '] img.inverterArrayImg').attr('src', resourceBaseUrl + '/' + platformUrl + '/img/monitor/plant/arrow/_arrow_left.gif').show();
