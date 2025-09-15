666c2ff9eda6a1db6bba7e9cd09994c18335430a
TrinhHieu9x <trinhdinhhieu96@gmail.com>
2025-09-15 21:27:26 +0700

    Update inverter_20240906.js

--- WManage_files/inverter_20240906.js
+++ WManage_files/inverter_20240906.js
@@ -599,7 +599,7 @@ function refreshPageAt1Minute() {   // Vòng nặng 20–30s
   if (typeof (refreshTigoDataIfSystemLayoutValid) == "function") {
     refreshTigoDataIfSystemLayoutValid();
   }
-  refreshInverterQuick(currentSerialNum);
+
   setTimeout(refreshPageAt1Minute, (showParallelData || redisRunning) ? (20 * 1000) : (30 * 1000));
 }
 // --- Gọi nhanh mỗi 5s, dữ liệu công suất từ web B ---
