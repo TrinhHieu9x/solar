00f648d5989a7c80bfe2b29b04d5db078fd7754a
TrinhHieu9x <trinhdinhhieu96@gmail.com>
2025-09-24 01:16:30 +0700

    Update inverter_20240906.js

--- WManage_files/inverter_20240906.js
+++ WManage_files/inverter_20240906.js
@@ -2125,7 +2125,7 @@ $('#enlargeChart').click(function () {
 	$('#lineChartContent').css('height', '600px')
 	$('#powerLineHolder').css('height', '560px')
 	var width = document.getElementById('lineChartContent').offsetWidth
-	powerChart.setSize(width-"5%", null)
+	powerChart.setSize(width-"8%", null)
 	$('#rightContent').hide()
 	$('#enlargeChart').hide()
 	$('#narrowChart').show()
@@ -2135,7 +2135,7 @@ $('#narrowChart').click(function () {
 	$('#lineChartContent').css('height', '400px')
 	$('#powerLineHolder').css('height', '360px')
 	var width = document.getElementById('lineChartContent').offsetWidth
-	powerChart.setSize(width-"10%", null)
+	powerChart.setSize(width-"15%", null)
 	$('#enlargeChart').show()
 	$('#narrowChart').hide()
 	$('#rightContent').show()
